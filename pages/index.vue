945a780c14a2087128450ef3d25ae376a6ba5639
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-08-07 15:43:02 +0200

    Freepik attribution added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Tutorial />
+
+    <a href="https://www.freepik.com/vectors/flower"
+      >Flower vector created by rawpixel.com - www.freepik.com</a
+    >
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
