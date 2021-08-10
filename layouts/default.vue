aa92c38e1ba0d1809a19cd90611170c3074149f5
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-08-10 14:58:37 +0200

    Data capture form HTML added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row bg-gray-100 h-screen">
+  <div class="flex flex-row bg-gray-100 h-full">
     <Menu class="flex-none w-100" />
     <Nuxt class="flex-grow" />
   </div>
