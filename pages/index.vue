2db3db17c4b97df3234d92751f6a3f52b190fbf8
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-08-10 17:56:01 +0200

    Data update via form to vuex

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <form class="p-20 space-y-8 divide-y divide-gray-200">
+  <form
+    @submit.prevent="submit"
+    class="p-20 space-y-8 divide-y divide-gray-200"
+  >
     <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
       <div>
         <div>
@@ -34,6 +37,8 @@
                 name="bride-name"
                 id="bride-name"
                 autocomplete="bride-name"
+                required
+                v-model="form.names.bride"
                 class="
                   max-w-lg
                   block
@@ -71,6 +76,8 @@
                 name="husband-name"
                 id="husband-name"
                 autocomplete="husband-name"
+                required
+                v-model="form.names.groom"
                 class="
                   max-w-lg
                   block
@@ -108,6 +115,47 @@
                 name="date"
                 type="date"
                 autocomplete="date"
+                required
+                v-model="form.date"
+                class="
+                  max-w-lg
+                  block
+                  w-full
+                  shadow-sm
+                  focus:ring-indigo-500
+                  focus:border-indigo-500
+                  sm:max-w-xs
+                  sm:text-sm
+                  border-gray-300
+                  rounded-md
+                "
+              />
+            </div>
+          </div>
+
+          <div
+            class="
+              sm:grid sm:grid-cols-3
+              sm:gap-4
+              sm:items-start
+              sm:border-t sm:border-gray-200
+              sm:pt-5
+            "
+          >
+            <label
+              for="time"
+              class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              Time
+            </label>
+            <div class="mt-1 sm:mt-0 sm:col-span-2">
+              <input
+                id="time"
+                name="time"
+                type="time"
+                autocomplete="time"
+                required
+                v-model="form.time"
                 class="
                   max-w-lg
                   block
@@ -145,6 +193,8 @@
                 name="address-line-one"
                 id="address-line-one"
                 autocomplete="address-line-one"
+                v-model="form.address.first"
+                required
                 class="
                   max-w-lg
                   block
@@ -181,7 +231,9 @@
                 type="text"
                 name="address-line-two"
                 id="address-line-two"
+                required
                 autocomplete="address-line-two"
+                v-model="form.address.second"
                 class="
                   max-w-lg
                   block
@@ -204,7 +256,7 @@
     <div class="pt-5">
       <div class="flex justify-end">
         <button
-          type="button"
+          type="reset"
           class="
             bg-white
             py-2
@@ -248,3 +300,32 @@
     </div>
   </form>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        names: {
+          bride: "",
+          groom: "",
+        },
+        date: null,
+        time: null,
+        address: {
+          first: "",
+          second: "",
+        },
+      },
+    };
+  },
+  methods: {
+    submit() {
+      this.$store
+        .dispatch("storeWeddingDetails", this.form)
+        .then(() => this.$router.push("/view"));
+    },
+  },
+};
+</script>
--- store/index.js
+++ store/index.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export const state = () => ({
     names: {
         bride: "Eve",
@@ -23,9 +25,30 @@ export const getters = {
 };
 
 export const mutations = {
-
+    updateNames(state, names) {
+        state.names = names;
+        return state.names;
+    },
+    updateDates(state, weddingDate, weddingTime) {
+        const date = moment(weddingDate);
+        state.date.day = date.format('dddd').toUpperCase();
+        state.date.month = date.format('MMM').toUpperCase();
+        state.date.date = date.format('D');
+        state.date.year = date.format('YYYY');
+        state.date.time = moment(weddingTime).format('H A');
+        return state.date;
+    },
+    updateAddress(state, address) {
+        state.address.first = address.first;
+        state.address.second = address.second;
+        return state.address;
+    }
 };
 
 export const actions = {
-
+    storeWeddingDetails({ commit }, formData) {
+        commit('updateNames', formData.names);
+        commit('updateDates', formData.date, formData.time);
+        return commit('updateAddress', formData.address);
+    }
 };
